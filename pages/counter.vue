9365c7c8b22fe7a656901917e729f0201a45dfc5
inosy22 <inos.sj@gmail.com>
2019-11-15 17:47:12 +0900

    :sparkles: vue側だけのサンプルのカウンターを実装

---
+++ pages/counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <p>Vue.js counter:「{{ counter }}」</p>
+      <button @click="increment">increment</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  components: {}
+})
+class Click extends Vue {
+  counter = 0
+  computed() {
+    return {
+      counter() {
+        return this.counter
+      }
+    }
+  }
+  increment() {
+    this.counter++
+  }
+}
+export default Click
+</script>
